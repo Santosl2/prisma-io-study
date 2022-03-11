f83a56fd22c15c36a3d7b4863989e10742a78d81
Santosl2c <mfilype2019@gmail.com>
2022-03-11 18:36:26 -0300

    :sparkles: Deliveryman, Clients, Deliveries migration

--- prisma/migrations/20220311212727_create_deliveryman_table/migration.sql
+++ prisma/migrations/20220311212727_create_deliveryman_table/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
-CREATE TABLE "Deliveryman" (
+CREATE TABLE "deliveryman" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
-    CONSTRAINT "Deliveryman_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "deliveryman_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Deliveryman_username_key" ON "Deliveryman"("username");
+CREATE UNIQUE INDEX "deliveryman_username_key" ON "deliveryman"("username");
